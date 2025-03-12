53c146f279efb23bbc5f90ba54e8fcc6aaeca7a5
TejashwiniU-U <14444teju@gmail.com>
2025-03-12 00:32:46 -0700

    Revert "sopln statement added"

--- New_Dummy_Project/src/main/java/com/comcast/orm/objectrepositoryUtiity/OpportunitiesPage.java
+++ New_Dummy_Project/src/main/java/com/comcast/orm/objectrepositoryUtiity/OpportunitiesPage.java
@@ -15,7 +15,6 @@ public class OpportunitiesPage {
 	@FindBy(xpath="//img[@title='Create Opportunity...']")
 	private WebElement newOpptuBtn;
 	public WebElement getNewOpptuBtn() {
-		System.out.println("create opprtuntiy page is created");
 		return newOpptuBtn;
 	}
 	
